02c9fb43897c7682310fea6390b00f19b16d5c60
basceb <sebastianbceblano@gmail.com>
2021-09-02 16:14:24 +0800

    assets commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 
-const App =() => <div className="App"> Hello World</div>
+const App =() => (
+    <>
+    <Navbar/>
+    <main style={{minHeight: "93vh"}}>
+
+    </main>
+    <Footer/>
+    </>
+    );
 
 export default App;
