ca0a08f63969bfca9e6f784cb3fe2abdb9bbf0a2
basceb <sebastianbceblano@gmail.com>
2021-09-14 16:18:07 +0800

    Login Backend Fix

---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets_pocketdevs/assets/css/animate.css'
+import './assets_pocketdevs/assets/css/bootstrap-5.0.0-beta1.min.css'
+import './assets_pocketdevs/assets/css/glightbox.min.css'
+import './assets_pocketdevs/assets/css/LineIcons.2.0.css'
+import './assets_pocketdevs/assets/css/main.css'
+import './assets_pocketdevs/assets/css/main.css.map'
+import './assets_pocketdevs/assets/css/tiny-slider.css'
+import 'react-bootstrap'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
