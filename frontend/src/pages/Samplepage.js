8a739353d43096ce50850792aa3e525061f834e1
Lester Niel <lesterfonggi123@gmail.com>
2021-09-16 22:47:30 +0800

    sidetask

--- frontend/src/pages/Samplepage.js
+++ frontend/src/pages/Samplepage.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 // import Navigation from "../components/Navigation";
 import Sidetask from "../components/Sidetask";
-import TaskManagement from "../components/TaskManagement";
 
 const Samplepage = () => {
   const [showNav, setShowNav] = useState(false);
 
   return (
     <div>
-      <TaskManagement />
       {/* <Navigation /> */}
       <header>
         <button onClick={() => setShowNav(!showNav)} className="theme-btn my-2">
