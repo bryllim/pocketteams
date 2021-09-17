0b81eed5dc640bc80b8d3c5ff60e4276b7d7531e
Lester Niel <lesterfonggi123@gmail.com>
2021-09-17 20:58:26 +0800

    notes and task management into board

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -4,17 +4,13 @@ import myNotes from "../assets_pocketdevs/assets/img/logo/note-icon.svg";
 
 const Notes = () => {
   return (
-    <div className="container">
-      <div className="text-center">
-        <h3 className="my-2 py-2 border-bottom border-bottom-dark">My Notes</h3>
-        <textarea className="border-1 resize-0 h6 h-full"></textarea>
-      </div>
-      <div>
-        <button className="theme-btn subtask py-0 d-flex align-items-center">
-          <Image className="fas-icon pt-2" src={myNotes}></Image>
-          Sync notes
-        </button>
-      </div>
+    <div class="sidebar-box recent-blog-box mb-30">
+      <h5>My Notes</h5>
+      <textarea cols="35" rows="12"></textarea>
+      <button className="theme-btn subtask py-0 d-flex align-items-center">
+        <Image className="fas-icon pt-2" src={myNotes}></Image>
+        Sync notes
+      </button>
     </div>
   );
 };
--- frontend/src/components/Taskcard.js
+++ frontend/src/components/Taskcard.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import Sidetask from "../components/Sidetask";
 
 const Taskcard = () => {
-    return (
-        <div className="d-flex flex-column mb-2 basecard">
-            <h6>Title</h6>
-            <p>Description</p>
-            <div className="d-flex justify-content-between align-items-center">
-                <p>date</p>
-                <div className="d-flex align-items-center">
-
-                    <button class="btn p-1 " type="button">
-                        <i class="bi bi-plus-circle"></i>
-                    </button>
-                    <img src="https://via.placeholder.com/100" alt="" className="rounded-circle" style={{width:'auto', height:'20px'}}/>             
-                   
-                </div>
-               
-            </div>
-            
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <div
+      onClick={() => setShowNav(!showNav)}
+      className="d-flex flex-column mb-2 basecard"
+    >
+      <Sidetask show={showNav} hide={setShowNav} />
+      <h6>Title</h6>
+      <p>Description</p>
+      <div className="d-flex justify-content-between align-items-center">
+        <p>date</p>
+        <div className="d-flex align-items-center">
+          <button class="btn p-1 " type="button">
+            <i class="bi bi-plus-circle"></i>
+          </button>
+          <img
+            src="https://via.placeholder.com/100"
+            alt=""
+            className="rounded-circle"
+            style={{ width: "auto", height: "20px" }}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Taskcard
+export default Taskcard;
