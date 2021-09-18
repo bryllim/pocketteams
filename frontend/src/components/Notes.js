db7162267c146c86ca1997369a3937e22c061ac0
basceb <sebastianbceblano@gmail.com>
2021-09-18 20:46:59 +0800

    Notes Fixed

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -6,7 +6,8 @@ const Notes = () => {
   return (
     <div class="sidebar-box recent-blog-box mb-30">
       <h5>My Notes</h5>
-      <textarea cols="35" rows="12"></textarea>
+      <hr className="notes"></hr>
+      <textarea cols="30" rows="15" className="notes-text"></textarea>
       <button className="theme-btn subtask py-0 d-flex align-items-center">
         <Image className="fas-icon pt-2" src={myNotes}></Image>
         Sync notes
