5b8bf7fa92d742624e3d9185c301a8b5c43a2db8
cdgmx <iamcdgmx@gmail.com>
2021-09-22 18:47:51 +0800

    Section

--- frontend/src/components/Sectioncard.jsx
+++ frontend/src/components/Sectioncard.jsx
@@ -6,30 +6,29 @@ const SectionCard = ({ taskList }) => {
   return (
     <>
    
-    <div className="d-flex flex-column section-wrapper mx-2" >
+    <div className="d-flex flex-column section-wrapper mx-2 mb-3" >
       <div className="d-flex justify-content-between align-items-center px-3 py-2 ">
 
         <h5 className="text-white">Sectionname</h5>
         <div>
           <button class="btn text-white" type="button">
-            <i class="lni lni-plus"></i>
+            <i class="lni lni-plus fs-3 "></i>
           </button>
       
           <button class="btn text-white" type="button">
-            <i class="lni lni-more-alt"></i>
+            <i class="lni lni-more-alt fs-3 "></i>
           </button>
         </div>
       
       </div>
-          
-
             <div className="section-wrapper-internal scrolling-wrapper-y flex-nowrap py-4 basecard">
 
             <TaskCard/>
-           
-         
-       
-            
+            <TaskCard/>
+            <TaskCard/>
+            <TaskCard/>
+            <TaskCard/>
+
                   <div class="d-flex justify-content-center theme-btn align-items-center mx-auto"  style={{width:"324px", height:"50px"}}>
                       <button class="btn" type="button">
                           <i class="lni lni-plus"></i>
