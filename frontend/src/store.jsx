c2ab0dea4fdc1d21a2143c40923df43b52402afa
Lester Niel <lesterfonggi123@gmail.com>
2021-10-27 17:27:00 +0800

    imported reducers

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -2,12 +2,8 @@ import { createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
-<<<<<<< HEAD
-import { projectCreateReducer, projectListReducer, projectUpdateReducer } from './reducers/projectReducers';
 import { noteListReducer, noteCreateReducer, noteUpdateReducer } from './reducers/noteReducers';
-=======
 import { projectCreateReducer, projectListReducer, projectUpdateReducer, projectDeleteReducer } from './reducers/projectReducers';
->>>>>>> 887a38e7d17d64ee38b6c495a4b848a52eb709b0
 
 const reducer = combineReducers({
     userLogin: userLoginReducer,
@@ -15,13 +11,10 @@ const reducer = combineReducers({
     projectList: projectListReducer,
     projectCreate: projectCreateReducer,
     projectUpdate: projectUpdateReducer,
-<<<<<<< HEAD
     noteList: noteListReducer,
     noteCreate: noteCreateReducer,
     noteUpdate: noteUpdateReducer,
-=======
     projectDelete: projectDeleteReducer
->>>>>>> 887a38e7d17d64ee38b6c495a4b848a52eb709b0
 });
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
