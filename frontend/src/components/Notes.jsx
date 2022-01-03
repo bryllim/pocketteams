270112e300e5e79b084706947427ea6e78076bff
bryllim <bryllim@gmail.com>
2022-01-04 07:08:46 +0800

    Adjusted notes

--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux"
-import { listNotes, createNoteAction, updateNoteAction } from "../actions/noteActions"
+import { useDispatch, useSelector } from "react-redux";
+import {
+  listNotes,
+  createNoteAction,
+  updateNoteAction,
+} from "../actions/noteActions";
 import Preload from "../components/Preload";
 import ErrorMessage from "../components/ErrorMessage";
 import { useHistory } from "react-router";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 toast.configure();
 
@@ -19,29 +23,27 @@ const Notes = () => {
   const { userInfo } = userLogin;
   const history = useHistory();
 
-  const [content, setContent] = useState(notes)
-  const [notif, setNotif] = useState()
-  
+  const [content, setContent] = useState(notes);
+  const [notif, setNotif] = useState();
+
   const noteUpdate = useSelector((state) => state.noteUpdate);
   const { success: successUpdate } = noteUpdate;
 
   const noteCreate = useSelector((state) => state.noteCreate);
   const { success: successCreate } = noteCreate;
 
-  const notify = () => toast.success(notif, {
-    position: toast.POSITION.BOTTOM_RIGHT,
-    autoClose: 2500,
-    
-  });
+  const notify = () =>
+    toast.success(notif, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: 2500,
+    });
 
   useEffect(() => {
     dispatch(listNotes());
     if (!userInfo) {
       history.push("/");
     }
-
   }, [dispatch, history, userInfo, successCreate, successUpdate]);
-
 
   // const defaultContent = 0;
 
@@ -56,8 +58,7 @@ const Notes = () => {
   //   fetching();
   // }, [note._id]);
 
-
-// for updating note
+  // for updating note
   // const updateHandler = (e) => {
   //   e.preventDefault();
   //   if (!content) return
@@ -66,53 +67,48 @@ const Notes = () => {
   //   window.location.reload(false);
   // }
 
-  //for creating note
+  // For creating notes
   const syncHandler = (e) => {
-    const defaultContent = "Write your note here."
+    const defaultContent = "Write your note here.";
     e.preventDefault();
     if (notes.length === 0) {
-      dispatch(createNoteAction( defaultContent ));
-      setNotif("Notes Created Successfully.")
-      notify()
-      window.location.reload(false)
-    } 
+      dispatch(createNoteAction(defaultContent));
+      setNotif("Notes Created Successfully.");
+      notify();
+      window.location.reload(false);
+    }
     if (notes.length >= 1) {
-      dispatch(updateNoteAction( notes[0]._id, content ));
-      setNotif("Notes Updated Successfully.")
-      notify()
-      }
-}
+      dispatch(updateNoteAction(notes[0]._id, content));
+      setNotif("Notes Updated Successfully.");
+      notify();
+    }
+  };
   // console.log(window.location.pathname);
 
   return (
     <div className="sidebar-box recent-blog-box mb-30">
-        <form>
-            <h5>My Notes</h5>
-            <hr className="default"></hr>
-            { error && <ErrorMessage variant='danger'>{error}</ErrorMessage>}
-            { loading && <Preload/> }
-            {notes?.map((note,index) => (
-              <textarea 
-              key={index}
-              note={note.content}
-              placeholder="Write your notes here."
-              onChange={(e) => setContent(e.target.value)} 
-              defaultValue={note.content}
-              // value={content}
-              className="notes-text"
-              ></textarea>
-            ))} 
-            <div>
-                {/* <button className="theme-btn theme-btn-md me-2"
-                onClick={createHandler}>
-                <span>Create note</span>
-                </button>  */}
-                <button className="theme-btn theme-btn-md" onClick={syncHandler}>
-                <i className="lni lni-cloud-sync me-3"></i>
-                <span>Sync notes</span>
-                </button> 
-            </div>
-        </form>
+      <form>
+        <h5>My Notes</h5>
+        <hr className="default"></hr>
+        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {loading && <Preload />}
+        {notes?.map((note, index) => (
+          <textarea
+            key={index}
+            note={note.content}
+            placeholder="Write your notes here..."
+            onChange={(e) => setContent(e.target.value)}
+            defaultValue={note.content}
+            className="notes-text"
+          ></textarea>
+        ))}
+        <div>
+          <button className="theme-btn theme-btn-md" onClick={syncHandler}>
+            <i className="lni lni-cloud-sync me-3"></i>
+            <span>Sync notes</span>
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
